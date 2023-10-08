96997ac2cd02c5984aed3afabf7908698d35f7ed
juandagongoradiaz <68030722+QuantumBreak3678@users.noreply.github.com>
2023-10-08 03:02:29 -0500

    Walk

--- Assets/Entrega/_Scripts/PlayerController.cs
+++ Assets/Entrega/_Scripts/PlayerController.cs
@@ -19,22 +19,49 @@ public class PlayerController : MonoBehaviour
     {
         GameObject tempPlayer = GameObject.FindGameObjectWithTag("Player");
         _charController = tempPlayer.GetComponent<CharacterController>();
-        _animator = tempPlayer.transform.GetChild(0).GetComponent<Animator>(); 
+        _animator = tempPlayer.transform.GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         inputX = Input.GetAxis("Horizontal");
-        inputZ = Input.GetAxis("Vertical"); 
+        inputZ = Input.GetAxis("Vertical");
+
+        if (inputZ == 0)
+        {
+            _animator.SetBool("isRun", false);
+
+            if (inputX > 0 || inputX < 0)
+            {
+                _animator.SetBool("isWalk", true);
+            }
+            else
+            {
+                _animator.SetBool("isWalk", false);
+            }
+        }
+        else
+        {
+            _animator.SetBool("isRun", true);
+        }
     }
 
     private void FixedUpdate()
     {
+        if (_charController.isGrounded)
+        {
+            v_velocity.y = 0f;
+        }
+        else
+        {
+            v_velocity.y -= gravity * Time.deltaTime; 
+        }
         v_movement = _charController.transform.forward * inputZ; 
         
         _charController.transform.Rotate(Vector3.up * inputX * (100f * Time.deltaTime));
 
         _charController.Move(v_movement * moveSpeed * Time.deltaTime);
+        _charController.Move(v_velocity);
     }
 }
