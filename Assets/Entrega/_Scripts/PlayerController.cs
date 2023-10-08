71549d9121af2bd0d59d8179ffde0a8bf587d3a4
juandagongoradiaz <68030722+QuantumBreak3678@users.noreply.github.com>
2023-10-08 04:27:50 -0500

    Blend v2

--- Assets/Entrega/_Scripts/PlayerController.cs
+++ Assets/Entrega/_Scripts/PlayerController.cs
@@ -9,13 +9,14 @@ public class PlayerController : MonoBehaviour
 
     private float inputX;
     private float inputZ;
-    private Vector3 v_movement;
-    private Vector3 v_velocity;
     public float moveSpeed;
     public float runSpeedMultiplier = 2.0f; // Multiplicador de velocidad para correr
+    public float rotationSpeed = 100.0f; // Velocidad de rotación
     public float gravity;
 
     private bool isRunning = false; // Variable para rastrear si el personaje está corriendo
+    
+    private Vector3 v_velocity = Vector3.zero;
 
     void Start()
     {
@@ -40,45 +41,44 @@ public class PlayerController : MonoBehaviour
             isRunning = false;
         }
 
-        // Configura las animaciones según las entradas de movimiento
-        if (inputZ != 0)
+        // Configura los parámetros del Blend Tree
+        float speed = Mathf.Abs(inputZ);
+        float direction = inputZ > 0 ? inputX : -inputX; // Ajusta la dirección para caminar hacia atrás
+
+        _animator.SetFloat("Speed", speed);
+        _animator.SetFloat("Direction", direction);
+
+        // Configura las animaciones de correr si se está corriendo
+        if (isRunning)
         {
-            if (isRunning)
-            {
-                _animator.SetBool("isRun", true);
-                _animator.SetBool("isWalk", false);
-            }
-            else
-            {
-                _animator.SetBool("isRun", false);
-                _animator.SetBool("isWalk", true);
-            }
-        }
-        else
-        {
-            _animator.SetBool("isRun", false);
-            _animator.SetBool("isWalk", false);
+            _animator.SetFloat("Speed", 1.0f);
+            _animator.SetFloat("Direction", 1.0f);
         }
     }
-
     private void FixedUpdate()
     {
         if (_charController.isGrounded)
         {
-            v_velocity.y = 0f;
+            // Reinicia la velocidad vertical cuando está en el suelo
+            if (v_velocity.y < 0f)
+            {
+                v_velocity.y = -2f;
+            }
         }
         else
         {
+            // Aplica la gravedad cuando está en el aire
             v_velocity.y -= gravity * Time.deltaTime;
         }
-        v_movement = _charController.transform.forward * inputZ;
 
-        // Aplica el multiplicador de velocidad si el personaje está corriendo
-        float speed = isRunning ? moveSpeed * runSpeedMultiplier : moveSpeed;
+        // Calcula el movimiento del personaje
+        Vector3 v_movement = transform.forward * inputZ;
 
-        _charController.transform.Rotate(Vector3.up * inputX * (100f * Time.deltaTime));
+        // Aplica la rotación del personaje
+        transform.Rotate(Vector3.up * inputX * (rotationSpeed * Time.deltaTime));
 
-        _charController.Move(v_movement * speed * Time.deltaTime);
-        _charController.Move(v_velocity);
+        // Aplica el movimiento al CharacterController
+        _charController.Move(v_movement * moveSpeed * Time.deltaTime);
+        _charController.Move(v_velocity * Time.deltaTime);
     }
 }
